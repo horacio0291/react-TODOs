5d48aef8bb10f3fe7164751447a64289bbe5534c
Horacio Santiago Ortiz <horacio0291@gmail.com>
2022-08-14 18:21:59 -0300

    first commit

---
+++ src/08-useReducer/TodoApp.jsx
@@ -0,0 +1,38 @@
+import { useTodos } from '../hooks';
+import { TodoAdd } from './TodoAdd';
+import { TodoList } from './TodoList';
+
+
+export const TodoApp = () => {
+
+    const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo } = useTodos();
+    
+    return (
+        <>
+            <h1>Tareas: { todosCount }, <small>pendientes: { pendingTodosCount }</small> </h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-sm-12 col-md-7">
+                    <TodoList
+                        todos={ todos } 
+                        onDeleteTodo={ handleDeleteTodo } 
+                        onToggleTodo={ handleToggleTodo }
+                    />
+                </div>
+
+                <div className="col-sm-12 col-md-5">
+                    <h4>Agregar Tarea</h4>
+                    <hr />
+                    <TodoAdd 
+                        onNewTodo={ handleNewTodo }  
+                    />
+                </div>
+
+            </div>
+
+
+        
+        </>
+    )
+}
